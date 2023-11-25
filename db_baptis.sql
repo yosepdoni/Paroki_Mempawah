a334cfd2708ce1302f40215e0d14f3e595422b6d
yosepdoni <yosepdoni2905@gmail.com>
2023-11-26 03:20:27 +0700

    penambahan table absen

--- db_baptis.sql
+++ db_baptis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Nov 2023 pada 17.20
+-- Waktu pembuatan: 25 Nov 2023 pada 21.19
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -24,11 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `absen`
+--
+
+CREATE TABLE `absen` (
+  `id_absen` int(5) NOT NULL,
+  `id_user` int(5) NOT NULL,
+  `kehadiran` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `absen`
+--
+
+INSERT INTO `absen` (`id_absen`, `id_user`, `kehadiran`) VALUES
+(1, 2, '0');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `baptis`
 --
 
 CREATE TABLE `baptis` (
   `id_baptis` int(5) NOT NULL,
+  `id_user` varchar(5) NOT NULL,
   `jenis_baptis` varchar(10) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `tgl_lahir` date NOT NULL,
@@ -44,12 +64,14 @@ CREATE TABLE `baptis` (
 -- Dumping data untuk tabel `baptis`
 --
 
-INSERT INTO `baptis` (`id_baptis`, `jenis_baptis`, `nama`, `tgl_lahir`, `tempat_lahir`, `nama_ortu`, `alamat`, `telepon`, `gambar`, `keterangan`) VALUES
-(7, 'dewasa', 'Daniel', '2023-11-13', 'Tayan', '0', 'hilir', 2147483647, '', ''),
-(8, 'dewasa', 'Suhendra', '2023-11-13', 'Pontianak', '0', 'Kota baru', 2147483647, 'IMG_20230323_151705.jpg', ''),
-(9, 'bayi', 'azril', '2023-11-13', 'Pontianak', 'Niko', 'Bangkam', 2147483647, '', ''),
-(11, 'dewasa', 'Daniel', '2023-11-13', 'Mempawah', '', 'Bangkam', 2147483647, 'IMG_20230323_223705.jpg', ''),
-(12, 'bayi', 'azril', '2023-11-13', 'Pontianak', 'Niko', 'hilir', 2147483647, '', '');
+INSERT INTO `baptis` (`id_baptis`, `id_user`, `jenis_baptis`, `nama`, `tgl_lahir`, `tempat_lahir`, `nama_ortu`, `alamat`, `telepon`, `gambar`, `keterangan`) VALUES
+(7, '', 'dewasa', 'Daniel', '2023-11-13', 'Tayan', '0', 'hilir', 2147483647, '', ''),
+(8, '', 'dewasa', 'Suhendra', '2023-11-13', 'Pontianak', '0', 'Kota baru', 2147483647, 'IMG_20230323_151705.jpg', ''),
+(9, '', 'bayi', 'azril', '2023-11-13', 'Pontianak', 'Niko', 'Bangkam', 2147483647, '', ''),
+(11, '', 'dewasa', 'Daniel', '2023-11-13', 'Mempawah', '', 'Bangkam', 2147483647, 'IMG_20230323_223705.jpg', ''),
+(12, '', 'bayi', 'azril', '2023-11-13', 'Pontianak', 'Niko', 'hilir', 2147483647, '', ''),
+(13, '', 'dewasa', 'Yosep Doni Saputra', '2023-11-26', 'Ampuk', '', 'Pontianak Timur', 2147483647, '', ''),
+(16, '4', 'dewasa', 'gerry topher', '2023-11-26', 'Tayan Pulau', '', 'Jl Penjara', 2147483647, '', '');
 
 -- --------------------------------------------------------
 
@@ -108,7 +130,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama`, `email`, `password`, `level`) VALUES
-(2, 'yosep', 'yosep@gmail.com', 'f5bb0c8de146c67b44babbf4e6584cc0', 'user'),
+(2, 'yosep', 'yosep@gmail.com', 'f5bb0c8de146c67b44babbf4e6584cc0', 'katekis'),
 (3, 'daniel', 'daniel@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
 (4, 'gerry topher', 'gerry@gmail.com', 'f5bb0c8de146c67b44babbf4e6584cc0', 'user'),
 (5, 'suhendra montek', 'suhendra@gmail.com', 'f5bb0c8de146c67b44babbf4e6584cc0', 'user');
@@ -116,6 +138,12 @@ INSERT INTO `user` (`id_user`, `nama`, `email`, `password`, `level`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `absen`
+--
+ALTER TABLE `absen`
+  ADD PRIMARY KEY (`id_absen`);
 
 --
 -- Indeks untuk tabel `baptis`
@@ -146,10 +174,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `absen`
+--
+ALTER TABLE `absen`
+  MODIFY `id_absen` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT untuk tabel `baptis`
 --
 ALTER TABLE `baptis`
-  MODIFY `id_baptis` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_baptis` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT untuk tabel `katekumen`
