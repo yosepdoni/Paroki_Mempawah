571ca5845d55b7e845ba2ec16be9eb5287bc1a27
SuhendraGanteng <suhendraganteng06@gmail.com>
2023-12-07 01:30:00 +0700

    perbaharui laporan

--- db_baptis.sql
+++ db_baptis.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Des 2023 pada 16.41
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.0.30
+-- Generation Time: Dec 06, 2023 at 07:28 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `baptis_bayi`
+-- Table structure for table `baptis_bayi`
 --
 
 CREATE TABLE `baptis_bayi` (
@@ -36,24 +37,25 @@ CREATE TABLE `baptis_bayi` (
   `alamat` varchar(50) NOT NULL,
   `nama_ayah` varchar(50) NOT NULL,
   `nama_ibu` varchar(50) NOT NULL,
-  `telepon` varchar(12) NOT NULL,
+  `telepon` varchar(15) NOT NULL,
   `akta` text NOT NULL,
   `surat_nikah` text NOT NULL,
   `status` varchar(20) NOT NULL,
-  `keterangan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `keterangan` text NOT NULL,
+  `tgl_baptis` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `baptis_bayi`
+-- Dumping data for table `baptis_bayi`
 --
 
-INSERT INTO `baptis_bayi` (`id_baptis`, `id_user`, `nama`, `tgl_lahir`, `tempat_lahir`, `alamat`, `nama_ayah`, `nama_ibu`, `telepon`, `akta`, `surat_nikah`, `status`, `keterangan`) VALUES
-(3, 6, 'Ivana', '2023-12-04', 'bangkam', 'siantan', 'QA', 'QB', '0856757575', 'keep.PNG', 'pay.jpg', 'Belum dikonfirmasi', 'Belum dikonfirmasi');
+INSERT INTO `baptis_bayi` (`id_baptis`, `id_user`, `nama`, `tgl_lahir`, `tempat_lahir`, `alamat`, `nama_ayah`, `nama_ibu`, `telepon`, `akta`, `surat_nikah`, `status`, `keterangan`, `tgl_baptis`) VALUES
+(28, 6, 'Ivana', '2023-12-07', 'Mempawah', 'Bangkam', 'A', 'B', '+6285752005641', 'akta.png', 'akta.png', 'Diterima', 'yyy', '2023-12-07');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `baptis_dewasa`
+-- Table structure for table `baptis_dewasa`
 --
 
 CREATE TABLE `baptis_dewasa` (
@@ -66,20 +68,21 @@ CREATE TABLE `baptis_dewasa` (
   `telepon` varchar(15) NOT NULL,
   `akta` text NOT NULL,
   `status` varchar(20) NOT NULL,
-  `keterangan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `keterangan` text NOT NULL,
+  `tgl_baptis` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `baptis_dewasa`
+-- Dumping data for table `baptis_dewasa`
 --
 
-INSERT INTO `baptis_dewasa` (`id_baptis`, `id_user`, `nama`, `tgl_lahir`, `tempat_lahir`, `alamat`, `telepon`, `akta`, `status`, `keterangan`) VALUES
-(22, 4, 'gerry topher', '2023-12-01', 'Tayan Pulau', 'Pontianak Timur', '85753613718', 'keep.PNG', 'Belum dikonfirmasi', 'Belum dikonfirmasi');
+INSERT INTO `baptis_dewasa` (`id_baptis`, `id_user`, `nama`, `tgl_lahir`, `tempat_lahir`, `alamat`, `telepon`, `akta`, `status`, `keterangan`, `tgl_baptis`) VALUES
+(26, 4, 'gerry topher', '2000-01-09', 'Tayan', 'Tayan', '+6285752005661', 'akta.png', 'Diterima', 'yaa', '2023-12-08');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jadwal`
+-- Table structure for table `jadwal`
 --
 
 CREATE TABLE `jadwal` (
@@ -90,20 +93,19 @@ CREATE TABLE `jadwal` (
   `tempat` varchar(50) NOT NULL,
   `perayaan` varchar(50) NOT NULL,
   `pastor` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `jadwal`
+-- Dumping data for table `jadwal`
 --
 
 INSERT INTO `jadwal` (`id_jadwal`, `hari`, `tgl`, `waktu`, `tempat`, `perayaan`, `pastor`) VALUES
-(4, 'Minggu', '2023-12-02', '06:00', 'Gereja St. Serenus Bangkam', 'Minggu Biasa XXVI', 'Yoseff'),
 (5, 'Minggu', '2023-12-01', '18:00', 'Gereja St. Serenus Bangkam', '-', 'Alex');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `katekumen`
+-- Table structure for table `katekumen`
 --
 
 CREATE TABLE `katekumen` (
@@ -113,21 +115,21 @@ CREATE TABLE `katekumen` (
   `tgl_lahir` date NOT NULL,
   `tempat_lahir` varchar(50) NOT NULL,
   `alamat` varchar(50) NOT NULL,
-  `telepon` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `telepon` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `katekumen`
+-- Dumping data for table `katekumen`
 --
 
 INSERT INTO `katekumen` (`id_katekumen`, `id_user`, `nama`, `tgl_lahir`, `tempat_lahir`, `alamat`, `telepon`) VALUES
-(20, 4, 'gerry topher', '2023-12-01', 'Tayan Pulau', 'Pontianak Timur', '85753613718'),
-(21, 5, 'suhendra montek', '2023-12-01', 'kobar', 'Pontianak Timur', '85753613718');
+(25, 4, 'gerry topher', '2000-01-09', 'Tayan', 'Tayan', '+6285752005661'),
+(26, 5, 'suhendra montek', '1999-09-18', 'kota baru', 'kota baru', '+6285752005555');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengumuman`
+-- Table structure for table `pengumuman`
 --
 
 CREATE TABLE `pengumuman` (
@@ -135,12 +137,12 @@ CREATE TABLE `pengumuman` (
   `id_baptis` int(5) NOT NULL,
   `status` varchar(20) NOT NULL,
   `keterangan` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `presensi`
+-- Table structure for table `presensi`
 --
 
 CREATE TABLE `presensi` (
@@ -148,26 +150,25 @@ CREATE TABLE `presensi` (
   `id_user` int(5) NOT NULL,
   `tgl_presensi` date NOT NULL,
   `jumlah_presensi` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `presensi`
+-- Dumping data for table `presensi`
 --
 
 INSERT INTO `presensi` (`id_presensi`, `id_user`, `tgl_presensi`, `jumlah_presensi`) VALUES
-(18, 4, '0000-00-00', ''),
-(25, 4, '2023-12-01', '1'),
-(26, 4, '2023-12-01', '1'),
-(27, 5, '0000-00-00', ''),
-(28, 5, '2023-12-01', '1'),
-(29, 4, '2023-12-04', '1'),
-(30, 4, '2023-12-04', '1'),
-(31, 4, '2023-12-04', '1');
+(35, 4, '0000-00-00', ''),
+(36, 5, '0000-00-00', ''),
+(37, 4, '2023-12-06', '1'),
+(38, 4, '2023-12-06', '1'),
+(39, 4, '2023-12-06', '1'),
+(40, 4, '2023-12-06', '1'),
+(41, 4, '2023-12-06', '1');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -176,10 +177,10 @@ CREATE TABLE `user` (
   `email` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
   `level` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `nama`, `email`, `password`, `level`) VALUES
@@ -194,89 +195,89 @@ INSERT INTO `user` (`id_user`, `nama`, `email`, `password`, `level`) VALUES
 --
 
 --
--- Indeks untuk tabel `baptis_bayi`
+-- Indexes for table `baptis_bayi`
 --
 ALTER TABLE `baptis_bayi`
   ADD PRIMARY KEY (`id_baptis`);
 
 --
--- Indeks untuk tabel `baptis_dewasa`
+-- Indexes for table `baptis_dewasa`
 --
 ALTER TABLE `baptis_dewasa`
   ADD PRIMARY KEY (`id_baptis`);
 
 --
--- Indeks untuk tabel `jadwal`
+-- Indexes for table `jadwal`
 --
 ALTER TABLE `jadwal`
   ADD PRIMARY KEY (`id_jadwal`);
 
 --
--- Indeks untuk tabel `katekumen`
+-- Indexes for table `katekumen`
 --
 ALTER TABLE `katekumen`
   ADD PRIMARY KEY (`id_katekumen`);
 
 --
--- Indeks untuk tabel `pengumuman`
+-- Indexes for table `pengumuman`
 --
 ALTER TABLE `pengumuman`
   ADD PRIMARY KEY (`id_pengumuman`);
 
 --
--- Indeks untuk tabel `presensi`
+-- Indexes for table `presensi`
 --
 ALTER TABLE `presensi`
   ADD PRIMARY KEY (`id_presensi`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `baptis_bayi`
+-- AUTO_INCREMENT for table `baptis_bayi`
 --
 ALTER TABLE `baptis_bayi`
-  MODIFY `id_baptis` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_baptis` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
--- AUTO_INCREMENT untuk tabel `baptis_dewasa`
+-- AUTO_INCREMENT for table `baptis_dewasa`
 --
 ALTER TABLE `baptis_dewasa`
-  MODIFY `id_baptis` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_baptis` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
--- AUTO_INCREMENT untuk tabel `jadwal`
+-- AUTO_INCREMENT for table `jadwal`
 --
 ALTER TABLE `jadwal`
   MODIFY `id_jadwal` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `katekumen`
+-- AUTO_INCREMENT for table `katekumen`
 --
 ALTER TABLE `katekumen`
-  MODIFY `id_katekumen` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_katekumen` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
--- AUTO_INCREMENT untuk tabel `pengumuman`
+-- AUTO_INCREMENT for table `pengumuman`
 --
 ALTER TABLE `pengumuman`
   MODIFY `id_pengumuman` int(5) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `presensi`
+-- AUTO_INCREMENT for table `presensi`
 --
 ALTER TABLE `presensi`
-  MODIFY `id_presensi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id_presensi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
